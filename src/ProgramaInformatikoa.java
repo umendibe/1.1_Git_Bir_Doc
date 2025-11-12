b7744aab65414701ef01d86e14a3d6c76b363f05
Urko <ikumendibe25@lhusurbil.eus>
2025-11-12 12:57:44 +0100

    1 kommita

---
+++ src/ProgramaInformatikoa.java
@@ -0,0 +1,20 @@
+public class ProgramaInformatikoa{
+    public static void main (String[] args){
+        System.out.println("1. GAIA");
+        System.out.println("Hau da nik ulertu dudana: ");
+        System.out.println("PROGRAMA INFORMATIKOA: ");
+        System.out.println("Programa informatikoa ordenagailu batek barneratu eta exekutatu ditzakeen adierazpenak dira. Normalean sekuentzialki exekutatzen dira baina zikloetan posible da ere bai.  ");
+        System.out.println("Softwere deritzo programa informatikoen multzoari eta haien adibide batzuk aplikazioak, sistema eragilea etab. izan daitezke.");
+        System.out.println("Software mota nagusiak honakoak dira: ");
+        System.out.printf("Aplikazioak: erabiltzaileentzako tresnak (testuak, kalkuluak, diseinua…).\n" + //
+                        "\n" + //
+                        "Pertsonalizatuak: neurrira egindakoak.\n" + //
+                        "\n" + //
+                        "Utilidadeak: mantentze eta laguntza tresnak.\n" + //
+                        "\n" + //
+                        "Sistema eragilea: hardwarea eta baliabideak kudeatzen ditu.\n" + //
+                        "\n" + //
+                        "Driver-ak: periferikoekin elkarreragina.");
+        System.out.println("");
+    }
+}
